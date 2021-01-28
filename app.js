28529d13841453cb6215a929e222ecfea37dd385
Fsesnick <fernandasesnick@gmail.com>
2021-01-28 11:07:43 +0000

    event bubbling and delegation final

--- app.js
+++ app.js
@@ -1,48 +1,33 @@
 const toggleList = document.getElementById('toggleList');
 const listDiv = document.querySelector('.list');
-
-
-//const input = document.querySelector('input');
-
-const input = document.querySelector('input.description');
-const p = document.querySelector('p.description');
-
-//const button = document.querySelector('button') ; gives us an error bc o toggle
-const button = document.querySelector('button.description') ;
-
+const descriptionInput = document.querySelector('input.description');
+const descriptionP = document.querySelector('p.description');
+const descriptionButton = document.querySelector('button.description');
 const addItemInput = document.querySelector('input.addItemInput');
 const addItemButton = document.querySelector('button.addItemButton');
 const removeItemButton = document.querySelector('button.removeItemButton');
-const listItems = document.getElementsByTagName('li');
 
-for (let i = 0; i < listItems.length; i += 1) {
-  listItems[i].addEventListener('mouseover', () => {
-    listItems[i].textContent = listItems[i].textContent.toUpperCase();
-  });
-  listItems[i].addEventListener('mouseout', () => {
-    listItems[i].textContent = listItems[i].textContent.toLowerCase();
-  });
-}
-
-toggleList.addEventListener('click', () =>{
-  if(listDiv.style.display == 'none'){
-  
-  toggleList.textContent = 'Ocultar lista';
-  listDiv.style.display= 'block';
-
-}else{
-  toggleList.textContent = 'Mostrar Lista';
-  listDiv.style.display = 'none';    
-}                      
+listDiv.addEventListener('mouseover', () => {
+  listItems[i].textContent = listItems[i].textContent.toUpperCase();
+});
+listDiv.addEventListener('mouseout', () => {
+  listItems[i].textContent = listItems[i].textContent.toLowerCase();
 });
 
-button.addEventListener('click', () => {
-  p.textContent = input.value + ';';
-  addItemInput.value = '';
-                        
+toggleList.addEventListener('click', () => {
+  if (listDiv.style.display == 'none') {
+    toggleList.textContent = 'Ocultar lista';
+    listDiv.style.display = 'block';
+  } else {
+    toggleList.textContent = 'Mostrar lista';                        
+    listDiv.style.display = 'none';
+  }                         
 });
 
-p.title = "List description";
+descriptionButton.addEventListener('click', () => {
+  descriptionP.innerHTML = descriptionInput.value + ':';
+  descriptionInput.value = '';
+});
 
 addItemButton.addEventListener('click', () => {
   let ul = document.getElementsByTagName('ul')[0];
@@ -51,13 +36,14 @@ addItemButton.addEventListener('click', () => {
   ul.appendChild(li);
   addItemInput.value = '';
 });
-
+  
 removeItemButton.addEventListener('click', () => {
   let ul = document.getElementsByTagName('ul')[0];
   let li = document.querySelector('li:last-child');
   ul.removeChild(li);
-  
 });
-
+  
+  
+  
   
   
